e029a4e1f3b455d92831661f01681dc6c2fc01ab
xu yaxiong <xyxlindy@163.com>
2023-08-16 23:16:50 +0800

    提交

--- uf_viz.py
+++ uf_viz.py
@@ -1,14 +1,5 @@
-from graphviz import Digraph
+import util
 
-ids = [6, 2, 6, 4, 4, 6, 6, 2, 4, 4, ]
+ids = [6, 2, 6, 4, 4, 6, 6, 2, 4, 4]
 
-g = Digraph('test', filename='test.gv')
-for index, id in enumerate(ids):
-    g.node(f'{index}', label=f'{index}')
-
-for index, id in enumerate(ids):
-    if index == id:
-        continue
-    g.edge(f'{index}', f'{id}')
-
-g.view()
+util.show_graph(ids)
--- util.py
+++ util.py
@@ -1,4 +1,5 @@
 from scipy.optimize import leastsq
+from graphviz import Digraph
 
 
 def err(p, x, y):
@@ -9,3 +10,17 @@ def myleastsq(xs, ys):
     p0 = [1, 1]
     ret = leastsq(err, p0, args=(xs, ys))
     return ret[0]
+
+
+def show_graph(ids):
+    g = Digraph("test", filename="test.gv")
+    g.graph_attr.update({"rankdir": "BT"})
+    for index, id in enumerate(ids):
+        g.node(f"{index}", label=f"{index}")
+
+    for index, id in enumerate(ids):
+        if index == id:
+            continue
+        g.edge(f"{index}", f"{id}")
+
+    g.view()
